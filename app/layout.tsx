9c233e0f8faffcd3b4a4f659fe15d05e9c19b2ff
Hameed Al-Naser <hameedalnaser@users.noreply.github.com>
2025-08-24 08:28:42 -0700

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/pics/icon.png" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9E6CFJYP9Z"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9E6CFJYP9Z');
+            `,
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
